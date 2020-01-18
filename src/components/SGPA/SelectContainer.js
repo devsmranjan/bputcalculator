f5a5c3ff1fa790785fb2b8b6593815c02714dd26
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2020-01-18 10:18:27 +0530

    added poppins font

--- src/components/SGPA/SelectContainer.js
+++ src/components/SGPA/SelectContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Box, Grid, TextField, MenuItem, Button } from '@material-ui/core';
 import DoneAllIcon from '@material-ui/icons/DoneAll';
 
